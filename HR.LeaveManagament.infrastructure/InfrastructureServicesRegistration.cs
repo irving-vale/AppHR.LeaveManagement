35fb744315d2a0d07d0eccdf0a1663b51c9c82ef
Irving Valencia <75141924+irving-vale@users.noreply.github.com>
2025-07-23 16:58:37 -0600

    feat() se creo la infrastructura para agregar el servicio de mensajeria y de logger

---
+++ HR.LeaveManagament.infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            
+            return services;
+        }
+    }
+}
+
